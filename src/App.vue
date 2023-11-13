92c25f6792979a69c711214a213bd7b82f3f29a8
IIODERII <oderrisi@gmail.com>
2023-11-13 15:39:01 +0100

    added main tremplate

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <HeaderComponent />
+  <MainComponent />
 </template>
 
 <script>
 import HeaderComponent from "./components/HeaderComponent.vue";
+import MainComponent from "./components/main/MainComponent.vue";
 export default {
   components: {
     HeaderComponent,
+    MainComponent,
   },
 };
 </script>
---
+++ src/components/main/MainComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+    <div class="container p-5">
+      <select class="form-select mb-4 mx-3" aria-label="Default select example">
+        <option selected>Archetipi</option>
+      </select>
+
+      <div class="bg-white p-5">
+        <div>
+          <div class="bg-black text-white fw-bold p-3">found 100 cards</div>
+
+          <div class="row"></div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "MainComponent",
+};
+</script>
+
+<style lang="scss" scoped>
+@use "../../assets/styles/partials/variables" as *;
+
+main {
+  background-color: $primaryColor;
+  select {
+    max-width: 200px;
+  }
+}
+</style>
